c0dd2cebc243cfc1defcebbe018f37bc468253aa
Vishwas Singh <031vishwas@gmail.com>
2021-06-07 00:29:08 +0530

    added particles.js

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -4,6 +4,9 @@ const share = document.querySelector('.share-button');
 
 let likesCount = 0;
 
+// particle js configuration
+particlesJS.load("particles-js", "particlesjs-config.json");
+
 // like.addEventListener('click',()=>{
 //     likesCount = likesCount + 1;
 //     document.
