f242947f81c5ff4d0ecadd17fc28b662a17e155b
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-06-09 00:40:05 +0530

    Added select photo option

--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -29,7 +29,6 @@ search.addEventListener('focusout', () => {
 
 //implementing the add feed logic
 
-
 const feedInput = document.querySelector('.feed-input');
 const addFeed = document.querySelector('.add-feed-after-click');
 const closeAddFeed = document.querySelector('.close-feed');
@@ -41,3 +40,28 @@ feedInput.addEventListener('click', () => {
 closeAddFeed.addEventListener('click', () => {
     addFeed.style.visibility = 'hidden';
 })
+
+//selecting image from the system 
+
+const imageButton = document.querySelector('#img');
+const preview = document.querySelector('.preview');
+
+imageButton.addEventListener('change', (event) => {
+    updateImageDisplay();
+})
+
+
+function updateImageDisplay() {
+    const curFiles = imageButton.files;
+    if (curFiles.length !== 0) {
+        for (const file of curFiles) {
+            // para.textContent = `File name ${file.name}, file size ${returnFileSize(file.size)}.`;
+            const image = document.createElement('img');
+            image.src = URL.createObjectURL(file);
+            image.style.width = '40%';
+            image.style.alignSelf = 'center';
+            // image.style.padding = '1rem';
+            preview.appendChild(image);
+        }
+    }
+}
